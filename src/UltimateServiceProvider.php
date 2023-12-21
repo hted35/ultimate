d9035d4ffbfec872616aed6e3e01ae81385bc0c3
tulparstudyo <teknovipyazilim@gmail.com>
2023-12-21 19:59:02 +0300

    Update

--- src/UltimateServiceProvider.php
+++ src/UltimateServiceProvider.php
@@ -11,7 +11,7 @@ class UltimateServiceProvider extends ServiceProvider{
      */
     public function register(): void
     {
-        //self::setNewRoutes();
+
     }
     /**
      * Bootstrap any application services.
